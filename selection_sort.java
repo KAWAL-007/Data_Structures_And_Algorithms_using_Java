7dc45a0cbb53cff73e538cc8e7c464562333590a
Kawaljot Singh Khalsa <kawaljotsingh06@gmail.com>
2026-04-29 13:32:43 +0530

    Implement selection sort algorithm in Java

---
+++ selection_sort.java
@@ -0,0 +1,21 @@
+class Main {
+    public static void main(String[] args) {
+        int arr[] = {5, 2, 9, 1};
+
+        for(int i = 0; i < arr.length; i++) {
+            int min = i;
+
+            for(int j = i + 1; j < arr.length; j++) {
+                if(arr[j] < arr[min])
+                    min = j;
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        for(int num : arr)
+            System.out.print(num + " ");
+    }
+}
