05fba97c55cfdcb8abd6e51a5c53051fc81223ef
Kawaljot Singh Khalsa <kawaljotsingh06@gmail.com>
2026-06-04 19:26:38 +0530

    Add Floyd-Warshall algorithm implementation
    Implement Floyd-Warshall algorithm to find shortest paths.

---
+++ Floyd–Warshall_algo.java
@@ -0,0 +1,42 @@
+class Main {
+
+    static final int INF = 99999;
+    static final int V = 4;
+
+    static void floydWarshall(int graph[][]) {
+
+        int dist[][] = new int[V][V];
+
+        for (int i = 0; i < V; i++)
+            for (int j = 0; j < V; j++)
+                dist[i][j] = graph[i][j];
+
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+
+                    if (dist[i][k] + dist[k][j] < dist[i][j])
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++)
+                System.out.print(dist[i][j] + " ");
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int graph[][] = {
+            {0, 5, INF, 10},
+            {INF, 0, 3, INF},
+            {INF, INF, 0, 1},
+            {INF, INF, INF, 0}
+        };
+
+        floydWarshall(graph);
+    }
+}
